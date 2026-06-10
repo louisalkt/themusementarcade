d645510c774d67992b593bea7da1ef3978f2d33b
louisalkt <117546903+louisalkt@users.noreply.github.com>
2026-06-11 00:29:48 +0100

    Revert "testing page load"
    This reverts commit c89b4150e72c2d41190d5a66e1a8016ac192a0a1.

--- site.js
+++ site.js
@@ -1,6 +1,5 @@
 (function() {
   const page = document.body.dataset.page;
-  const BASE = '/themusementarcade'; // change this to your repo name if different
 
   function inject(url, selector, position) {
     fetch(url)
@@ -18,6 +17,6 @@
       });
   }
 
-  inject(BASE + '/_header.html', 'main', 'before');
-  inject(BASE + '/_footer.html', 'main', 'after');
+  inject('/_header.html', 'main', 'before');
+  inject('/_footer.html', 'main', 'after');
 })();
